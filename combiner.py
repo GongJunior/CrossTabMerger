73c892fb181e652c1be69b6b7fe5b74314486ec3
Anthony <GongJunior@users.noreply.github.com>
2017-11-06 21:54:15 -0500

    Update combiner.py
    Adding notes for futures upgrades

--- combiner.py
+++ combiner.py
@@ -6,8 +6,9 @@
 import pandas as pd
 import os
 
-pathVar = r'C:\Users\Anthony.Fields\Documents\Workbin\Bill Gates\gts' #change to correct path
-extVar = '.xlsx'#update to regex to take .xls also
+#update to take basic user input
+pathVar = r'' #path to file(s)
+extVar = '.xlsx'#update to all crosstab variations
 resultFile = 'result.xlsx' #name of file to be created
 startrow = 10 #what row to start collecting data, 0 is default
 sheet = 'Compensation Data' #0 = default
@@ -42,13 +43,7 @@ del result
 print('Done!')
 os.system("pause")
 
-# import pandas as pd
-# import os
-# 
-# pathVar = r'C:\Users\anthony.fields\Documents\Workbin\indeed' #change to correct path
-# extVar = '.xlsx'#update to regex to take .xls also
-# resultFile = '2017 GSS.xlsx' #name of file to be created
-# startrow = 8 #what row to start collecting data, 0 is default
+# futures update for taking worksheets as well as workbooks
 # filename = '2017_July_GSS.xlsx'
 # sheetList = ['IND Hyderabad','IND Bangalore','IND','IRL Dublin','IRL','DEU','DEU Dusseldorf','BRA','BRA Sao Paulo','AUS','AUS Sydney','CAN','CAN Toronto','JPN','JPN Tokyo','US, SW and Revenue Cut','US National','US Phoenix','US Seattle','US NY Metro','US SF Bay','US Austin','UK National'
 # ]
