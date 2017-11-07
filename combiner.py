fc42a43e5535fafb2ce5bc5dbc33e8e89fd46204
Anthony <GongJunior@users.noreply.github.com>
2017-11-06 22:52:55 -0500

    Update combiner.py
    start changing to functions for main input file

--- combiner.py
+++ combiner.py
@@ -1,44 +1,43 @@
 #! python3
 # coding: utf-8
 
-# In[ ]:
+def excel_mix(pathVar,startrow,sheet):
+	import pandas as pd
+	import os
 
-import pandas as pd
-import os
+	#variables to take from user input
+	#pathVar = r'' #path to file(s)
+	#extVar = '.xlsx'#update to all crosstab variations
+	#startrow = 10 #what row to start collecting data, 0 is default
+	#sheet = 'Compensation Data' #0 = default
+	resultFile = 'result.xlsx' #name of file to be created
 
-#update to take basic user input
-pathVar = r'' #path to file(s)
-extVar = '.xlsx'#update to all crosstab variations
-resultFile = 'result.xlsx' #name of file to be created
-startrow = 10 #what row to start collecting data, 0 is default
-sheet = 'Compensation Data' #0 = default
-
-#build list of files to combine
-print('Gathering files...')
-fileList = []
-for filename in os.listdir(pathVar):
-	if filename.endswith(extVar):
-		fileList.append(filename)
-		print('%s added to list' %(filename))
-
+	#build list of files to combine
+	print('Gathering files...')
+	fileList = []
+	for filename in os.listdir(pathVar):
+		if filename.endswith('.xlsx') or filename.endswith('.xls'):
+			fileList.append(filename)
+			print('%s added to list' %(filename))
 
 
-#create data frame for each file
-#combine dataframes into one
-start = startrow -1
-frames = [ pd.read_excel(os.path.join(pathVar,f),skiprows=start,sheetname=sheet) for f in fileList ]
-#frames = [ pd.read_csv(os.path.join(pathVar,f),encoding='cp1252') for f in fileList ]
-number = len(fileList)
+
+	#create data frame for each file
+	#combine dataframes into one
+	start = startrow -1
+	frames = [ pd.read_excel(os.path.join(pathVar,f),skiprows=start,sheetname=sheet) for f in fileList ]
+	#frames = [ pd.read_csv(os.path.join(pathVar,f),encoding='cp1252') for f in fileList ]
+	number = len(fileList)
 
 
-print('Combining %s files, please be patient :)' %(number))
-result = pd.concat(frames)
-del frames
-#print combined dataframes into one exceel sheet
-result.to_excel(os.path.join(pathVar,resultFile),sheet_name='ALLINFO',index=False)
-del result
-print('Done!')
-os.system("pause")
+	print('Combining %s files, please be patient :)' %(number))
+	result = pd.concat(frames)
+	del frames
+	#print combined dataframes into one exceel sheet
+	result.to_excel(os.path.join(pathVar,resultFile),sheet_name='ALLINFO',index=False)
+	del result
+	print('Done!')
+	os.system("pause")
 
 # futures update for taking worksheets as well as workbooks
 # filename = '2017_July_GSS.xlsx'
